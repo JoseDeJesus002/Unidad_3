3ea7af5eb34c282a03299cb91f5cc0dbdf739a7e
JoseDeJesus002 <jose_1321124119@uptecamac.edu.mx>
2021-11-23 17:33:35 -0600

    Modificacion

--- New/Program.cs
+++ New/Program.cs
@@ -4,23 +4,17 @@
     {
         static void Main(string[] args)
         {
-            //int x=0;
             int y=0;
             int z=0;
             Random rnd = new Random();
             Console.WriteLine("Ingrese el numero de valores a crear: ");
             z =Int32.Parse(Console.ReadLine());
-            //Console.WriteLine("Ingrese el valor de x min");
-            //x = Int32.Parse(Console.ReadLine());
-            Console.WriteLine("Ingrese el valor de y max");
+            Console.WriteLine("Ingrese el rango maximo de numeros: ");
             y = Int32.Parse(Console.ReadLine());
-           
-            //Console.WriteLine("The Random Number is: "+ranNum);
             for(int i = 0; i <= z; i++)
             {
                 int ranNum = rnd.Next(1, y);
-                Console.WriteLine("Este es su numero: " + ranNum);
-            }
-            
+                Console.WriteLine("Este es el numero: " + ranNum);
+            }   
+        }
     }
-}
