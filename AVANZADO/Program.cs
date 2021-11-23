96575621397b1648ebc4a174448c554702522486
JoseDeJesus002 <jose_1321124119@uptecamac.edu.mx>
2021-11-23 15:42:16 -0600

    Random

--- AVANZADO/Program.cs
+++ AVANZADO/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace MyApp
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // --Se llaman listas 
+            // estos son arreglos de tipo string
+            int[] elements = {1,2,3,4,5,6,7,8,9,10};
+            String [] names = {"Juan","Pedro","Maria","Jose","Luis","Ana","Jorge","Sofia","Sara","Rosa"};
+            hello(" Jose " , 18);
+
+            // Esto es parecido a un .push en javascript
+            List<int> elementsList = new List<int>();
+            elementsList.Add(1);
+            elementsList.Add(2);
+
+            //Es para generar una funcion con el void y el nombre de la funcion
+            static void hello(string nombre, int edad)
+            {
+                Console.WriteLine(" Hello World! "+ nombre + " edad " + edad );
+            }
+        }
+    }
+}
---
+++ New/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int x=0;
+            int y=0;
+            int z=0;
+            Random rnd = new Random();
+            Console.WriteLine("Ingrese el numero de valores a crear: ");
+            z =Int32.Parse(Console.ReadLine());
+            //Console.WriteLine("Ingrese el valor de x min");
+            //x = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el valor de y max");
+            y = Int32.Parse(Console.ReadLine());
+           
+            //Console.WriteLine("The Random Number is: "+ranNum);
+            for(int i = 0; i <= z; i++)
+            {
+                int ranNum = rnd.Next(1, y);
+                Console.WriteLine("Este es su numero: " + ranNum);
+            }
+            
+    }
+}
