d976cf5ee997cc9c4649107e0347cd48459ccf70
JoseDeJesus002 <jose_1321124119@uptecamac.edu.mx>
2021-11-24 18:48:36 -0600

    Java y C#

---
+++ Jva/tablero.js
@@ -0,0 +1,29 @@
+//var preguntar_tablero = prompt("¿Quieres que el tablero sea inicializado?");
+var tablero = new Array(10);
+var tablero1= new Array(10);
+var n ="";
+var x = "";
+
+function barco(){
+aleert("Bienvenido al juego de batlla naval")
+nombres();
+alert("muy bien");
+tableros();
+}
+function names(){
+    j1=prompt("Ingresa el nombre del jugador 1");
+    j2=prompt("Ingresa el nombre del jugador 2");
+    n.push(j1);
+    x.push(j2);
+    alert("Bienvenido "+j1+" y "+j2);   
+}
+function tableros(){
+for(var i = 0; i < 10; i++){
+
+    
+    
+    }
+}
+
+
+    
